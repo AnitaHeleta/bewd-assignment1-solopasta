7acb36861c0a0f708d1050bf9ffc74e7bb675be6
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-09-01 00:45:00 +0200

    Styled a bit login,register,change password pages

--- Public/templates/header.php
+++ Public/templates/header.php
@@ -1,30 +1,17 @@
-<!doctype html>
 <?php
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__ . "/common.php");
 session_start();
 ensure_loggedIn();
 ?>
-<html lang="en">
-<head>
-    <title>Solo Pasta</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href=<?php normalize_url("assets/css/style.css") ?>/>
-    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo normalize_url("assets/css/style.css") ?>">
-</head>
-
+<?php include "html_head.php" ?>
 <header>
-
     <a href="<?php echo normalize_url("index.php") ?>">
         <img id="logo" src="<?php echo normalize_url("assets/img/solopastalogo.png") ?>">
     </a>
-
     <h1>
         Welcome to Solo Pasta
     </h1>
-
     <div class="quote">
         <p>"life is a combination of magic and pasta"</p>
         <figure> Federico Fellini
---
+++ Public/templates/html_head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Solo Pasta</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href=<?php normalize_url("assets/css/style.css") ?>/>
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo normalize_url("assets/css/style.css") ?>">
+</head>
