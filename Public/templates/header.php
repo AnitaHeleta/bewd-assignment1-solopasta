6919deb5ac3e32cbec2169bc97793f55c7b16f2e
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2020-09-01 19:22:45 +0200

    remove div on logged in user

--- Public/templates/header.php
+++ Public/templates/header.php
@@ -13,12 +13,13 @@ ensure_loggedIn();
                 </a>
                 <p>"life is a combination of magic and pasta"</p>
                 <div id="user">
-                    <span><?php echo htmlspecialchars($_SESSION["username"]); ?></span>
-                    <div>
-                        <a class="glyphicon glyphicon-erase"
-                           href="<?php echo normalize_url("reset-password.php") ?>"></a>
-                        <a class="glyphicon glyphicon-log-out" href="<?php echo normalize_url("logout.php") ?>"></a>
-                    </div>
+                    <span id="username"><?php echo htmlspecialchars($_SESSION["username"]); ?></span>
+                    <a class="glyphicon glyphicon-erase"
+                       title="Reset Password"
+                       href="<?php echo normalize_url("reset-password.php") ?>"></a>
+                    <a class="glyphicon glyphicon-log-out"
+                       title="Logout"
+                       href="<?php echo normalize_url("logout.php") ?>"></a>
                 </div>
             </header>
         </div>
